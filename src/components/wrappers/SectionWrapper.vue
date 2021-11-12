68961c3aa97bf70eda17e46c35dd6254ef264249
jaschahuisman <jascha.huisman@gmail.com>
2021-11-12 11:58:52 +0100

    Added section animation

--- src/components/wrappers/SectionWrapper.vue
+++ src/components/wrappers/SectionWrapper.vue
@@ -1,12 +1,36 @@
 <script setup lang="ts">
-withDefaults(
+import { ref, onMounted } from "vue";
+import gsap from "@/vendors/gsap";
+
+const props = withDefaults(
   defineProps<{ title?: string; titleOffset?: string; animate?: boolean }>(),
   { title: undefined, titleOffset: undefined, animate: true },
 );
+
+const sectionRef = ref(null);
+
+onMounted(() => {
+  const sectionElement = sectionRef.value;
+  if (props.animate === true) {
+    gsap.from(sectionElement, {
+      opacity: 0,
+      y: 80,
+      scale: 0.95,
+      duration: 1,
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: sectionElement,
+        start: "top 90%",
+        end: "60% 30%",
+        scrub: true,
+      },
+    });
+  }
+});
 </script>
 
 <template>
-  <section ref="section" class="section" role="group">
+  <section ref="sectionRef" class="section" role="group">
     <h2
       v-if="title"
       class="section__title"
