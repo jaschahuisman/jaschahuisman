234f96ef486ee188899e454bb6c9e22f481af5a1
jaschahuisman <jascha.huisman@gmail.com>
2021-11-13 14:42:37 +0100

    💡 Added icon resolver

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import svgLoader from "vite-svg-loader";
+import Icons from "unplugin-icons/vite";
+
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
+  plugins: [vue(), svgLoader(), Icons({ compiler: "vue3" })],
 
   resolve: {
     alias: {
