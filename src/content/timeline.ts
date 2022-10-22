04dbc59b07c33136387233b64f91e66eb775cd1f
Jascha Huisman <jascha.huisman@gmail.com>
2022-10-22 12:52:33 +0200

    💅 Updated timeline

--- src/content/timeline.ts
+++ src/content/timeline.ts
@@ -1,6 +1,7 @@
 import AvansImage from "@/assets/timeline/avans.png";
 import HandpickedImage from "@/assets/timeline/handpicked.svg?url";
 import HkuImage from "@/assets/timeline/hku.svg?url";
+import LabImage from "@/assets/timeline/lab.svg?url";
 
 export const timelineItems: Array<{
   title: string;
@@ -9,23 +10,30 @@ export const timelineItems: Array<{
   image: string;
 }> = [
   {
-    title: "Avans University of Applied Sciences",
-    date: "2019",
-    description:
-      "Propaedeutic year Bachelor Communication and Multimedia Design",
-    image: AvansImage,
-  },
-  {
     title: "Handpicked Agencies",
     date: "2020",
     description:
-      "Tech elaboration internship & freelance at Handpicked Labs innovation centre",
+      "Tech elaboration internship and freelance work at Handpicked Lab.",
     image: HandpickedImage,
   },
   {
     title: "University of the Arts Utrecht",
     date: "2021",
-    description: "Minor Extended Reality",
+    description: "Minor Extended Reality.",
     image: HkuImage,
+  },
+  {
+    title: "Avans University of Applied Sciences",
+    date: "2022",
+    description:
+      "Cum laude Bachelor degree Communication and Multimedia Design.",
+    image: AvansImage,
+  },
+  {
+    title: "Handpicked Lab",
+    date: "2022",
+    description:
+      "Making new technology and innovation tangible at Handpicked Lab as Creative Technologist.",
+    image: LabImage,
   },
 ];
